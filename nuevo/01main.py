5e4107a5deb5a6a04f5f7b2b23b8c5920bdb754d
Leonardo Caliva <leocaliva@gmail.com>
2025-06-23 18:36:09 -0300

    temas preparado para seguir histograma retornos diarios

--- nuevo/01main.py
+++ nuevo/01main.py
@@ -1,6 +1,11 @@
 from descarga_datos import descargar_datos, limpiar_datos_crudos
 from analisis_retornos import calcular_retornos_diarios_acumulados
 
+# ---------------- CONFIGURACIÓN DE ACCIONES ----------------
+descargar = False       # Descargar nuevos datos desde el proveedor
+limpiar = False         # Limpiar y transformar los datos crudos descargados
+analizar = True         # Realizar análisis y gráficos
+
 # ---------------- CONFIGURACIÓN ----------------
 tickers = ["AAPL", "MSFT", "GOOGL"]
 start_date = "2000-01-01"
@@ -11,14 +16,14 @@ proveedor = "yahoo"   # 'alphavantage', 'tiingo' , 'yahoo'
 tema_grafico = "bloomberg_dark"         # 'dark', 'vintage', 'modern', 'normal'
 retorno_logaritmico = True     # True para logarítmico, False para simple
 
+
 activar_retornos_ventana = False
 ventanas_móviles = [5, 22, 252]  # Por defecto: semanal, mensual, anual
 
 # Opcional: calcular retornos por bloque (semana/mes/año)
-calcular_retornos_por_periodo = True
+calcular_retornos_por_periodo = False
 frecuencias_temporales = ["W-FRI", "ME", "YE"]  # semanal, mensual, anual
 
-# Configuración de referencias
 referencias_histograma = {
     "media": True,
     "sigma": True,
@@ -31,23 +36,22 @@ referencias_histograma = {
     "p99": False
 }
 
-# ------------------------------------------------
+# ---------------- EJECUCIÓN DE ACCIONES ----------------
+if descargar:
+    descargar_datos(tickers, start_date, end_date, proveedor)
 
-# DESCARGA Y LIMPIEZA
-descargar_datos(tickers, start_date, end_date, proveedor)
-limpiar_datos_crudos()
+if limpiar:
+    limpiar_datos_crudos()
 
-# ANÁLISIS
-calcular_retornos_diarios_acumulados(
-    carpeta_datos_limpios="DatosLimpios",
-    carpeta_salida="RetornoDiarioAcumulado",
-    tema=tema_grafico,
-    logaritmico=retorno_logaritmico,
-    calcular_rolling=activar_retornos_ventana,
-    ventanas=ventanas_móviles,
-    calcular_bloques=calcular_retornos_por_periodo,
-    frecuencias=frecuencias_temporales,
-    referencias_histograma=referencias_histograma
-)
-
-
+if analizar:
+    calcular_retornos_diarios_acumulados(
+        carpeta_datos_limpios="DatosLimpios",
+        carpeta_salida="RetornoDiarioAcumulado",
+        tema=tema_grafico,
+        logaritmico=retorno_logaritmico,
+        calcular_rolling=activar_retornos_ventana,
+        ventanas=ventanas_móviles,
+        calcular_bloques=calcular_retornos_por_periodo,
+        frecuencias=frecuencias_temporales,
+        referencias_histograma=referencias_histograma
+    )
